0a0d2d120f1268e008d0d0711307db8d6c9c019e
Zach Yanez <zachyanez@gmail.com>
2021-01-05 22:33:53 -0600

    attempted deploy

--- user-directory/src/components/Cards/Cards.js
+++ user-directory/src/components/Cards/Cards.js
@@ -8,7 +8,7 @@ const Cards = (props) => {
     <img className="card-img-top" src={props.picture} alt="Card image cap"/>
     <div className="card-body">
     <h5 className="card-title"> {props.first} {props.last}</h5>
-    <p className="card-text">Phone # {props.phone}</p>
+    <p className="card-text">Phone #: {props.phone}</p>
     <p className="card-text">Email: {props.email}</p>
     <p className="card-text">state: {props.state}</p>
     </div>
