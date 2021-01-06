98b713bb23fcb283f0f748b4067fc868f38487f8
Zach Yanez <zachyanez@gmail.com>
2021-01-05 21:38:38 -0600

    Adding sorting buttons

--- user-directory/src/components/Navbar/Navbar.js
+++ user-directory/src/components/Navbar/Navbar.js
@@ -13,6 +13,12 @@ function Navbar(props) {
             <input className="form-control mr-sm-2" type="search" placeholder="Search by first name" aria-label="Search"id="first" onChange={props.handleChange}/>
             <input className="form-control mr-sm-2" type="search" id="last" placeholder="Search by last name" aria-label="Search" onChange={props.handleChange}/>
             </div>
+            <div className="justify-content-end m-2">
+            <button type="button" class="btn btn-primary m-2">First</button>
+            <button type="button" class="btn btn-primary m-2">Next</button>
+            <button type="button" class="btn btn-primary m-2">Last</button>
+            <button type="button" class="btn btn-primary m-2">Other</button>
+            </div>
         </nav>
 
         
